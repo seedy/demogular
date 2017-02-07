d1249faf4ce5ebbaf179254b9b95ab1d8f34febc
seedy.dupuis <seedy.dupuis@gmail.com>
2017-02-08 00:11:40 +0100

    feat : added grids in main, theming and colors in index scss

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 angular
-  .module('app', ['ui.router']);
+  .module('app', ['ui.router', 'ngMaterial'])
+  .config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('pink')
+    .accentPalette('orange');
+  });
